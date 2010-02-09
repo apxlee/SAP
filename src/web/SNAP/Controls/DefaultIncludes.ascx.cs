1dfefee9da6e53d459584fdae05293d8ad9c0d11
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-09 23:28:05 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4023 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/DefaultIncludes.ascx.cs
+++ src/web/SNAP/Controls/DefaultIncludes.ascx.cs
@@ -1,23 +1,17 @@
 ﻿using System;
-using System.Collections;
-using System.Configuration;
-using System.Data;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Security;
 using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Xml.Linq;
 
 namespace Apollo.AIM.SNAP.Web.Controls
 {
-    public partial class DefaultIncludes : System.Web.UI.UserControl
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class DefaultIncludes : System.Web.UI.UserControl
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
