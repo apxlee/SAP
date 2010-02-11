cdef7c94022d85fb65ce6adeb5418070d6a5cc70
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-11 22:48:14 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4071 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/UserView.ascx.cs
+++ src/web/SNAP/Controls/UserView.ascx.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Data;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Apollo.AIM.SNAP.Web.Common;
