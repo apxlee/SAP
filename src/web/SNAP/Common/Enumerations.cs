7d34676b96877dc2b96a34e0442426dd03b6c461
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-11 21:38:49 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4059 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/web/SNAP/Common/Enumerations.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Apollo.AIM.SNAP.Web.Common
+{
+	public enum Role
+	{
+		User,
+		ApprovingManager,
+		AccessTeam,
+		SuperUser
+	}
+	
+	public enum RequestState
+	{
+		Open,
+		Pending,
+		Change_Requested,
+		Closed
+	}
+	
+	public enum WorkflowState
+	{
+		Approved,
+		Change_Requested,
+		Closed_Cancelled,
+		Closed_Completed,
+		Closed_Denied,
+		Not_Active,
+		Pending_Acknowlegement,
+		Pending_Approval,
+		Pending_Provisioning,
+		Pending_Workflow,
+		Workflow_Created
+	}
+}
