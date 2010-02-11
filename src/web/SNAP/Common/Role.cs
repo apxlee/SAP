46c13138811e71fd1474a17af09bfaaefc761255
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-11 19:02:09 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4057 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/web/SNAP/Common/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Apollo.AIM.SNAP.Web.Common
+{
+	public enum Role
+	{
+		User,
+		ApprovingManager,
+		AccessTeam
+	}
+}
