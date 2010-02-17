c54b22154abb7f9f6961063de9c1b92008b6ae4a
clschwim <clschwim@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-17 22:10:27 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4100 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/lib/Apollo.AIM.SNAP/Model/RequestData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+
+namespace Apollo.AIM.SNAP.Model
+{
+    public class RequestData
+    {
+        public string FormId { get; set; } 
+        public string UserText { get; set; }
+        public static string ToXml(List<RequestData> data)
+        {
+            var root = new XElement("ROOT");
+            foreach (var list in data)
+            {
+                var requestData = new XElement("request_data", new XAttribute("access_details_formId", list.FormId), new XAttribute("userText", list.UserText));
+                root.Add(requestData);
+            }
+
+            return root.ToString();
+        }
+    }
+}
