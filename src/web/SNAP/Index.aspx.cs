56324a90fa902a309f1fb2770f3347655314b59a
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-18 20:58:58 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4110 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Index.aspx.cs
+++ src/web/SNAP/Index.aspx.cs
@@ -5,15 +5,21 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+
 using Apollo.CA.Logging;
 using Apollo.Ultimus.CAP;
 using Apollo.Ultimus.CAP.Model;
 using Apollo.Ultimus.Web;
+using Apollo.AIM.SNAP.Web.Common;
 
 namespace Apollo.AIM.SNAP.Web
 {
 	public partial class Index : Page
 	{
-		protected void Page_Load(object sender, EventArgs e)	{ }
+		protected void Page_Load(object sender, EventArgs e)	
+		{
+			// TODO: utility to find AD group and/or look into DB for existence of pending approvals to determine role
+			Page.Session["SNAPUserRole"] = Role.Requestor;
+		}
 	}
 }
