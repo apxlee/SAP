6ba31dadd684013ec2fad1c637d010e0a9c73635
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-23 21:32:44 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4137 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/web/SNAP/Controls/Header.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Header.ascx.cs" Inherits="Apollo.AIM.SNAP.Web.Controls.Header" %>
+<div class="snap_brand_header" style="height:94px;">
+	<div class="csm_container_16" style="height:94px;background: url('../images/tlrTOP.png') top repeat-y;">
+		<div class="csm_grid_4 csm_prefix_2 csm_alpha csm_omega logo_itservices"></div>
+		<div class="csm_grid_6 csm_prefix_3 csm_alpha csm_omega logo_oospa"></div>
+		<div class="csm_clear">&nbsp;</div>
+		<asp:PlaceHolder ID="_ribbonContainer" runat="server"></asp:PlaceHolder>
+	</div>
+</div>
+<div class="csm_clear">&nbsp;</div>
---
+++ src/web/SNAP/Controls/Header.ascx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Apollo.AIM.SNAP.Web.Controls
+{
+	public partial class Header : System.Web.UI.UserControl
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			BuildRibbon("my_requests");
+
+		}
+
+		private PlaceHolder WrapControl(WebControl control, string element, string elementID)
+		{
+			PlaceHolder placeHolder = new PlaceHolder();
+
+			Literal openElement = new Literal();
+			openElement.Text = string.Format("<{0} id='{1}'>", element, elementID);
+
+			Literal closeElement = new Literal();
+			closeElement.Text = "</" + element + ">";
+
+			placeHolder.Controls.Add(openElement);
+			placeHolder.Controls.Add(control);
+			placeHolder.Controls.Add(closeElement);
+
+			return placeHolder;
+		}		
+		
+		private void BuildRibbon(string CurrentPageName)
+		{
+			Panel ribbonPanel = new Panel();
+			ribbonPanel.CssClass = CurrentPageName;
+			
+			List<string> linkButtons = new List<string>
+			{
+				"request_form", "my_requests", "my_approvals", "access_team", "search", "support"
+			};
+
+			Literal openElements = new Literal();
+			openElements.Text = string.Format("<div id='{0}' class=''><ul id='{1}'>"
+				, "csm_ribbon_container_outer"
+				, "csm_ribbon_container_inner");
+
+			Literal closeElements = new Literal();
+			closeElements.Text = "</ul></div>";
+
+			ribbonPanel.Controls.Add(openElements);
+
+			foreach (string link_name in linkButtons)
+			{
+				LinkButton link = new LinkButton();
+				link.CommandName = link_name + "_button";
+				link.Command += new CommandEventHandler(RibbonLink_Click);
+				link.Text = link.CommandName;
+				ribbonPanel.Controls.Add(WrapControl(link, "li", link.CommandName));
+			}
+
+			ribbonPanel.Controls.Add(closeElements);			
+			
+			_ribbonContainer.Controls.Add(ribbonPanel);
+		}
+		
+        protected void RibbonLink_Click(object sender, CommandEventArgs e)
+        {
+        }
+	}
+}
