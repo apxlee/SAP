9ae04368c67dd6a174fb959010e585cd6bfd18ce
clschwim <clschwim@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-02-26 18:17:22 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4193 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/web/SNAP/scripts/MasterRequestBlade.js
@@ -0,0 +1,22 @@
+﻿function csmToggle(sender, tree) {
+    if (tree == "ppnn") {
+        $(document).ready(function() {
+            if ($(sender).parent().parent().next().next().is(":hidden")) {
+                $(sender).parent().parent().next().nextAll().slideDown("fast");
+                var $kid = $(sender).children(".csm_toggle_icon_down").removeClass("csm_toggle_icon_down");
+                $kid.addClass("csm_toggle_icon_up");
+            }
+            else {
+                $(sender).parent().parent().next().nextAll().slideUp("fast");
+                var $kid = $(sender).children(".csm_toggle_icon_up").removeClass("csm_toggle_icon_up");
+                $kid.addClass("csm_toggle_icon_down");
+            }
+        });
+    }
+    else {
+        $(document).ready(function() {
+            if ($(sender).next().is(":hidden")) { $(sender).next().slideDown("fast"); }
+            else { $(sender).next().slideUp("fast"); }
+        });
+    }
+}
