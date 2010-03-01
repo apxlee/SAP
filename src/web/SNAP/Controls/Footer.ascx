dfb516518d56ac95b2ed3af4644a441998df82a3
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-03-01 23:24:53 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4205 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/Footer.ascx
+++ src/web/SNAP/Controls/Footer.ascx
@@ -3,9 +3,31 @@
 <div class="csm_container_100 csm_footer_float">
 	<div class="csm_footer snap_brand_footer">
 		<div class="csm_container_16">
-			<div class="csm_grid_10 csm_prefix_1">
-				<p>heading links duplicated</p>
-				<p>additional footer links (contact, feedback, metrics)</p>
+			<div class="csm_grid_3 csm_prefix_2 csm_top10">
+				<h1>Application Links</h1>
+				<ul>
+					<li><a href="#">Request Form</a></li>
+					<li><a href="#">Search</a></li>
+					<li><a href="#">Support</a></li>
+				</ul>
+			</div>
+			<div class="csm_grid_3 csm_top10">
+				<h1>Demonstration Role Change</h1>
+				<ul>
+					<li><a href="index.aspx?role=SuperUser">Super User</a></li>
+					<li><a href="index.aspx?role=ApprovingManager">Approving Manager</a></li>
+					<li><a href="index.aspx?role=AccessTeam">Access Team</a></li>
+					<li><a href="index.aspx?role=Requestor">Requestor (Default User)</a></li>
+					<li><a href="index.aspx?role=NotAuthenticated">Not Authenticated</a></li>
+				</ul>
+			</div>
+			<div class="csm_grid_2 csm_top10">
+				<h1>Feedback</h1>
+				<ul>
+					<li><a href="#">Comments</a></li>
+					<li><a href="#">Report Problem</a></li>
+					<li><a href="#">Statistics</a></li>
+				</ul>
 			</div>
 			<div class="csm_grid_4 csm_alpha csm_omega csm_footer_logo"></div>
 			<div class="csm_clear">&nbsp;</div>
--- src/web/SNAP/Controls/Header.ascx.cs
+++ src/web/SNAP/Controls/Header.ascx.cs
@@ -16,10 +16,6 @@ namespace Apollo.AIM.SNAP.Web.Controls
 		
 		protected void Page_Load(object sender, EventArgs e)
 		{
-			// TODO: for testing
-			//PendingApprovals = 10;
-			//PendingAccessTeam = 20;
-			
 			BuildRibbon();
 		}
 
@@ -42,11 +38,8 @@ namespace Apollo.AIM.SNAP.Web.Controls
 		
 		private void BuildRibbon()
 		{
-			//_ribbonContainerOuter.CssClass = CurrentPageName;
-			
 			// TODO: build from xml?  make List of objects (link buttons?) that have url pre-populated?
 			//
-			
 			List<string> linkButtons = new List<string>{};
 			string linksContainerWidth = "763";
 			
@@ -54,12 +47,12 @@ namespace Apollo.AIM.SNAP.Web.Controls
 			{
 				case Role.ApprovingManager :
 					linkButtons.AddRange(new List<string> { "request_form", "my_requests", "my_approvals", "search", "support" });
-					linksContainerWidth = "763";
+					linksContainerWidth = "600";
 					break;
 					
 				case Role.AccessTeam :
 					linkButtons.AddRange(new List<string> { "request_form", "my_requests", "access_team", "search", "support" });
-					linksContainerWidth = "763";
+					linksContainerWidth = "600";
 					break;
 					
 				case Role.SuperUser :
@@ -69,12 +62,12 @@ namespace Apollo.AIM.SNAP.Web.Controls
 
 				case Role.Requestor :
 					linkButtons.AddRange(new List<string> { "request_form", "my_requests", "search", "support" });
-					linksContainerWidth = "763";
+					linksContainerWidth = "430";
 					break;
 				
 				case Role.NotAuthenticated :
 				default :
-					linkButtons.AddRange(new List<string>{ "login" });
+					linkButtons.AddRange(new List<string>{ "login", "support" });
 					break;
 			}
 
@@ -86,7 +79,7 @@ namespace Apollo.AIM.SNAP.Web.Controls
 			foreach (string link_name in linkButtons)
 			{
 				LinkButton link = new LinkButton();
-				link.CommandName = link_name; // + "_button";
+				link.CommandName = link_name;
 				link.Command += new CommandEventHandler(RibbonLink_Click);
 				
 				switch (link_name)
@@ -107,7 +100,6 @@ namespace Apollo.AIM.SNAP.Web.Controls
 				}
 
 				_ribbonContainerOuter.Controls.Add(WrapControl(link, "li", link.CommandName));
-				//_ribbonContainerOuter.CssClass = link_name;
 			}
 
 			Literal closeElements = new Literal();
@@ -117,54 +109,8 @@ namespace Apollo.AIM.SNAP.Web.Controls
 		
         protected void RibbonLink_Click(object sender, CommandEventArgs e)
         {
-			//int viewIndex = (int)Enum.Parse(typeof(ViewIndex), e.CommandName);
 			WebUtilities.SetActiveView( (int)Enum.Parse(typeof(ViewIndex), e.CommandName) );
 			_ribbonContainerOuter.CssClass = e.CommandName;
-        
-			//MultiView viewControl;
-			//viewControl = (MultiView)WebUtilities.FindControlRecursive(Page, "_masterMultiView");
-			
-			// TODO: make the view index an enum or object and eliminate need for silly switch
-			// or use setactiveview method in webutils
-			//
-			//switch (e.CommandName)
-			//{
-			//    case "request_form":
-			//        WebUtilities.SetActiveView(1);
-			//        //viewControl.ActiveViewIndex = 1;
-			//        _ribbonContainerOuter.CssClass = "request_form";
-			//        break;
-
-			//    case "my_requests":
-			//        viewControl.ActiveViewIndex = 2;
-			//        _ribbonContainerOuter.CssClass = "my_requests";
-			//        break;
-
-			//    case "my_approvals":
-			//        viewControl.ActiveViewIndex = 3;
-			//        _ribbonContainerOuter.CssClass = "my_approvals";
-			//        break;
-
-			//    case "access_team":
-			//        viewControl.ActiveViewIndex = 4;
-			//        _ribbonContainerOuter.CssClass = "access_team";
-			//        break;
-
-			//    case "search":
-			//        viewControl.ActiveViewIndex = 5;
-			//        _ribbonContainerOuter.CssClass = "search";
-			//        break;
-
-			//    case "support":
-			//        viewControl.ActiveViewIndex = 6;
-			//        _ribbonContainerOuter.CssClass = "support";
-			//        break;
-					
-			//    default :
-			//        viewControl.ActiveViewIndex = 0;
-			//        _ribbonContainerOuter.CssClass = "";
-			//        break;
-			//}
         }
 	}
 }
