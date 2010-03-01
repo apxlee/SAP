4028f94e3db593434656f3c4ba89d923c09dba32
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-03-01 20:54:16 +0000

    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4201 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/Header.ascx.cs
+++ src/web/SNAP/Controls/Header.ascx.cs
@@ -17,8 +17,8 @@ namespace Apollo.AIM.SNAP.Web.Controls
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			// TODO: for testing
-			PendingApprovals = 10;
-			PendingAccessTeam = 20;
+			//PendingApprovals = 10;
+			//PendingAccessTeam = 20;
 			
 			BuildRibbon();
 		}
@@ -44,28 +44,49 @@ namespace Apollo.AIM.SNAP.Web.Controls
 		{
 			//_ribbonContainerOuter.CssClass = CurrentPageName;
 			
-			// TODO: based on role, build list of linkbuttons
 			// TODO: build from xml?  make List of objects (link buttons?) that have url pre-populated?
 			//
-			List<string> linkButtons = new List<string>
+			
+			List<string> linkButtons = new List<string>{};
+			string linksContainerWidth = "763";
+			
+			switch ( WebUtilities.CurrentRole )
 			{
-				"request_form", "my_requests", "my_approvals", "access_team", "search", "support"
-			};
+				case Role.ApprovingManager :
+					linkButtons.AddRange(new List<string> { "request_form", "my_requests", "my_approvals", "search", "support" });
+					linksContainerWidth = "763";
+					break;
+					
+				case Role.AccessTeam :
+					linkButtons.AddRange(new List<string> { "request_form", "my_requests", "access_team", "search", "support" });
+					linksContainerWidth = "763";
+					break;
+					
+				case Role.SuperUser :
+					linkButtons.AddRange(new List<string> { "request_form", "my_requests", "my_approvals", "access_team", "search", "support" });
+					linksContainerWidth = "763";
+					break;
+
+				case Role.Requestor :
+					linkButtons.AddRange(new List<string> { "request_form", "my_requests", "search", "support" });
+					linksContainerWidth = "763";
+					break;
+				
+				case Role.NotAuthenticated :
+				default :
+					linkButtons.AddRange(new List<string>{ "login" });
+					break;
+			}
 
 			Literal openElements = new Literal();
 			openElements.Text = string.Format("<div id='{0}' style='width:{1}px;'><ul id='{2}'>"
-				, "csm_ribbon_container_outer", "763", "csm_ribbon_container_inner");
-			// TODO: dynamically figure out proper width based on number of links
-
-			Literal closeElements = new Literal();
-			closeElements.Text = "</ul></div>";
-
+				, "csm_ribbon_container_outer", linksContainerWidth, "csm_ribbon_container_inner");
 			_ribbonContainerOuter.Controls.Add(openElements);
 
 			foreach (string link_name in linkButtons)
 			{
 				LinkButton link = new LinkButton();
-				link.CommandName = link_name + "_button";
+				link.CommandName = link_name; // + "_button";
 				link.Command += new CommandEventHandler(RibbonLink_Click);
 				
 				switch (link_name)
@@ -86,55 +107,64 @@ namespace Apollo.AIM.SNAP.Web.Controls
 				}
 
 				_ribbonContainerOuter.Controls.Add(WrapControl(link, "li", link.CommandName));
+				//_ribbonContainerOuter.CssClass = link_name;
 			}
 
+			Literal closeElements = new Literal();
+			closeElements.Text = "</ul></div>";
 			_ribbonContainerOuter.Controls.Add(closeElements);		
 		}
 		
         protected void RibbonLink_Click(object sender, CommandEventArgs e)
         {
-			MultiView viewControl;
-			viewControl = (MultiView)WebUtilities.FindControlRecursive(Page, "_masterMultiView");
+			//int viewIndex = (int)Enum.Parse(typeof(ViewIndex), e.CommandName);
+			WebUtilities.SetActiveView( (int)Enum.Parse(typeof(ViewIndex), e.CommandName) );
+			_ribbonContainerOuter.CssClass = e.CommandName;
+        
+			//MultiView viewControl;
+			//viewControl = (MultiView)WebUtilities.FindControlRecursive(Page, "_masterMultiView");
 			
 			// TODO: make the view index an enum or object and eliminate need for silly switch
+			// or use setactiveview method in webutils
 			//
-			switch (e.CommandName)
-			{
-				case "request_form_button":
-					viewControl.ActiveViewIndex = 1;
-					_ribbonContainerOuter.CssClass = "request_form";
-					break;
+			//switch (e.CommandName)
+			//{
+			//    case "request_form":
+			//        WebUtilities.SetActiveView(1);
+			//        //viewControl.ActiveViewIndex = 1;
+			//        _ribbonContainerOuter.CssClass = "request_form";
+			//        break;
 
-				case "my_requests_button":
-					viewControl.ActiveViewIndex = 2;
-					_ribbonContainerOuter.CssClass = "my_requests";
-					break;
+			//    case "my_requests":
+			//        viewControl.ActiveViewIndex = 2;
+			//        _ribbonContainerOuter.CssClass = "my_requests";
+			//        break;
 
-				case "my_approvals_button":
-					viewControl.ActiveViewIndex = 3;
-					_ribbonContainerOuter.CssClass = "my_approvals";
-					break;
+			//    case "my_approvals":
+			//        viewControl.ActiveViewIndex = 3;
+			//        _ribbonContainerOuter.CssClass = "my_approvals";
+			//        break;
 
-				case "access_team_button":
-					viewControl.ActiveViewIndex = 4;
-					_ribbonContainerOuter.CssClass = "access_team";
-					break;
+			//    case "access_team":
+			//        viewControl.ActiveViewIndex = 4;
+			//        _ribbonContainerOuter.CssClass = "access_team";
+			//        break;
 
-				case "search_button":
-					viewControl.ActiveViewIndex = 5;
-					_ribbonContainerOuter.CssClass = "search";
-					break;
+			//    case "search":
+			//        viewControl.ActiveViewIndex = 5;
+			//        _ribbonContainerOuter.CssClass = "search";
+			//        break;
 
-				case "support_button":
-					viewControl.ActiveViewIndex = 6;
-					_ribbonContainerOuter.CssClass = "support";
-					break;
+			//    case "support":
+			//        viewControl.ActiveViewIndex = 6;
+			//        _ribbonContainerOuter.CssClass = "support";
+			//        break;
 					
-				default :
-					viewControl.ActiveViewIndex = 0;
-					_ribbonContainerOuter.CssClass = "";
-					break;
-			}
+			//    default :
+			//        viewControl.ActiveViewIndex = 0;
+			//        _ribbonContainerOuter.CssClass = "";
+			//        break;
+			//}
         }
 	}
 }
