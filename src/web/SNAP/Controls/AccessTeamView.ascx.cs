e6bc1a2ec69a6a5c888ded5ace6234882adcec08
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-03-12 21:27:50 +0000

    logic to render view only as needed
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4342 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/AccessTeamView.ascx.cs
+++ src/web/SNAP/Controls/AccessTeamView.ascx.cs
@@ -11,14 +11,20 @@ namespace Apollo.AIM.SNAP.Web.Controls
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			if (WebUtilities.CurrentViewIndex == ViewIndex.access_team)
+			{
+				BuildAccessTeamView();
+			}
+		}
+		
+		private void BuildAccessTeamView()
+		{
             var requestLoader = new Common.AccessTeamRequestLooder();
             requestLoader.Load();
 
             ViewBaseUtilities.BuildRequests(this.Page, RequestState.Open, _openRequestsContainer, _nullDataMessage_ClosedRequests, false);
             //ViewBaseUtilities.BuildRequests(this.Page, RequestState.Closed, _closedRequestsContainer, _nullDataMessage_ClosedRequests, true);
             ViewBaseUtilities.BuildRequests(this.Page, RequestState.Closed, _closedRequestsContainer, _nullDataMessage_ClosedRequests, false);
-             
 		}
-
 	}
 }
