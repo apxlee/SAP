61c5ee38b18b79568db2186011b0a00c07d7ad0b
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-04-08 23:50:46 +0000

    removed commented enumerations that are in snap.model now
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4477 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Common/Enumerations.cs
+++ src/web/SNAP/Common/Enumerations.cs
@@ -1,53 +1,6 @@
 ﻿
 namespace Apollo.AIM.SNAP.Web.Common
 {
-	public enum Role
-	{
-		NotAuthenticated, 
-		Requestor,
-		ApprovingManager,
-		AccessTeam,
-		SuperUser
-	}
-	
-    /*
-	public enum RequestState
-	{
-		Open = 0,
-		Change_Requested = 1,
-		Pending = 2,
-		Closed = 3
-	}
-	
-	public enum WorkflowState
-	{
-		Approved = 0,
-		Change_Requested = 1,
-		Closed_Cancelled = 2,
-		Closed_Completed = 3,
-		Closed_Denied = 4,
-		Not_Active = 5,
-		Pending_Acknowlegement = 6,
-		Pending_Approval = 7,
-		Pending_Provisioning= 8,
-		Pending_Workflow = 9,
-		Workflow_Created = 10
-	}
-	
-	public enum CommentsType
-	{
-		Denied = 1,
-		Cancelled = 2,
-		Requested_Change = 3,
-		Email_Reminder = 4,
-		Access_Notes_Requestor = 5,
-		Access_Notes_ApprovingManager = 6,
-		Access_Notes_AccessTeam = 7,		
-		Access_Notes_SuperUser = 9
-	}
-
-     */
-
 	public enum ViewIndex
 	{
 		login = 0,
