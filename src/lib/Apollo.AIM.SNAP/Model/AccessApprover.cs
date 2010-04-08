095cfde5d6b89ec545d695581b4187e73880b50e
clschwim <clschwim@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-04-08 23:25:40 +0000

    Added for Workflow builder section
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4464 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/lib/Apollo.AIM.SNAP/Model/AccessApprover.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apollo.AIM.SNAP.Model
+{
+    public class AccessApprover
+    {
+        public int ActorId { get; set; }
+        public string UserId { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsDefault { get; set; }
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public ActorApprovalType ActorApprovalType { get; set; }
+    }
+}
