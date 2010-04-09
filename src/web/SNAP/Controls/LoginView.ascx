5b3706a44559cb831d13c9184851505d9095c07c
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-04-09 00:03:07 +0000

    new view
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4483 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/web/SNAP/Controls/LoginView.ascx
@@ -0,0 +1,44 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="LoginView.ascx.cs" Inherits="Apollo.AIM.SNAP.Web.Controls.LoginView" %>
+<div class="csm_container_center_700">
+	<div class="csm_content_container">
+		
+		<asp:Panel ID="_loginMessageContainer" runat="server" CssClass="csm_content_container" Visible="false">
+			<!--<div class="csm_text_container">-->
+				<asp:Label ID="_loginMessage" runat="server"></asp:Label>
+			<!--</div>-->
+		</asp:Panel>
+	
+		<fieldset>
+		
+			<table border="0" cellpadding="0" cellspacing="0" class="csm_input_form_container">
+				<tr>
+					<td class="csm_input_form_label_column csm_input_required_field">
+						<label for="_loginViewControl__networkId">Network Login</label>
+					</td>
+					<td class="csm_input_form_control_column">
+						<asp:TextBox ID="_networkId" runat="server" CssClass="csm_text_input_short"></asp:TextBox>
+					</td>
+				</tr>
+
+				<tr>
+					<td class="csm_input_form_label_column csm_input_required_field">
+						<label for="_loginViewControl__password">Password</label>
+					</td>
+					<td class="csm_input_form_control_column">
+						<asp:TextBox TextMode="Password" ID="_password" runat="server" CssClass="csm_text_input_short" ></asp:TextBox>
+					</td>
+				</tr>
+			</table>
+		
+			<div class="csm_text_container">
+				<p>[TODO: Action checkboxes and verbiage]</p>
+			</div>	
+		
+			<div class="csm_input_buttons_container">
+				<asp:Button ID="_submitLogin" Text="Submit" CssClass="csm_html_button" runat="server" onclick="_submitLogin_Click" />
+			</div>
+		
+		</fieldset>				
+	
+	</div>
+</div>
---
+++ src/web/SNAP/Controls/LoginView.ascx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Apollo.Ultimus.Web;
+using Apollo.AIM.SNAP.Web.Common;
+
+namespace Apollo.AIM.SNAP.Web.Controls
+{
+	public partial class LoginView : System.Web.UI.UserControl
+	{
+		protected void Page_Load(object sender, EventArgs e) { }
+		
+		private void DisplayMessage(string message, bool isError)
+		{
+			_loginMessage.Text = message;
+			_loginMessageContainer.Visible = true;
+			
+			if (isError) _loginMessage.CssClass += " csm_error_text";
+		}
+		
+		protected void _submitLogin_Click(object sender, EventArgs e)
+        {
+			if (!IsAuthenticatedUser(_networkId.Text, _password.Text))
+			{
+				DisplayMessage(@"Unable to Login.  Please re-enter your Network Login and Password. [Authentication Failure]", true);
+			}
+			else
+			{
+				Session.Clear();
+				
+				// TODO: make sure networkid is validated client-side so no nulls here
+				string networkId = _networkId.Text.ToLower().Trim();
+
+				SnapUser currentUser = new SnapUser(networkId);
+				SnapSession.CurrentUser = currentUser;
+				
+				string redirectUrl = "index.aspx";
+				if (!string.IsNullOrEmpty(Request.QueryString.ToString()))
+				{
+					redirectUrl += "?" + Request.QueryString.ToString();
+				}
+				WebUtilities.Redirect(redirectUrl , false);
+			}
+        }
+        
+        private bool IsAuthenticatedUser(string networkId, string password)
+        {
+#if DEBUG
+	return true;
+#endif			
+			try
+			{
+				LdapAuthentication authentication = new LdapAuthentication();
+				
+				if (authentication.IsAuthenticated(networkId.ToLower().Trim(), password.Trim()))
+				{
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				// TODO: Logger.Error("LoginView > AuthenticateUser", ex);
+			}
+			
+			return false;
+        }
+	}
+}
