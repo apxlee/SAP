823c90403d6dbefd4fbf608d9651333a4562511f
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-04-16 18:46:48 +0000

    new file
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4541 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/web/SNAP/scripts/MyApprovals.js
@@ -0,0 +1,21 @@
+﻿//<![CDATA[
+function ApproveRequest(requestId) {
+
+	var postData = "{'requestId':'" + requestId.toString() + "'}";
+
+	$.ajax({
+		type: "POST",
+		contentType: "application/json; character=utf-8",
+		url: "AjaxCalls.aspx/ApproveRequest",
+		data: postData,
+		dataType: "json",
+		success: function(msg) { alert(msg.d); }
+		,
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("GetNames Error: " + XMLHttpRequest);
+			alert("GetNames Error: " + textStatus);
+			alert("GetNames Error: " + errorThrown);
+		}
+	});
+}
+//]]>
