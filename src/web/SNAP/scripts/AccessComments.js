93f32198c300f1d592a1fd1d4866ac2ac34183a6
clschwim <clschwim@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-04-21 17:54:32 +0000

    added js for comments section
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4576 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

---
+++ src/web/SNAP/scripts/AccessComments.js
@@ -0,0 +1,36 @@
+﻿//<![CDATA[
+
+function audienceClick(obj) {
+    $(obj).closest("table").next().find("input[type=button]").removeAttr("disabled");
+}
+
+function AccessComments(obj, requestId) {
+    var comments;
+    var action = $(obj).parent().prev().find("input[name=_audience]:checked").val();
+    var textarea = $(obj).parent().prev().find("textarea");
+
+    if (textarea.val() != "") {
+        comments = textarea.val();
+        var postData = "{'requestId':'" + requestId.toString() + "','action':'" + action + "','comments':'" + comments + "'}";
+        alert(postData);
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; character=utf-8",
+            url: "AjaxCalls.aspx/AccessComments",
+            data: postData,
+            dataType: "json",
+            success: function(msg) { alert(msg.d); }
+		,
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("GetNames Error: " + XMLHttpRequest);
+                alert("GetNames Error: " + textStatus);
+                alert("GetNames Error: " + errorThrown);
+            }
+        });
+    }
+    else {
+        alert("please insert comments");
+    }  
+}
+
+//]]>
