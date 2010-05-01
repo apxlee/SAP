e37bea80cf8afd3c9363da17788dde74563c1bae
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-05-01 23:57:39 +0000

    commented for line count, moved to default master page
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4703 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/LoginView.ascx.cs
+++ src/web/SNAP/Controls/LoginView.ascx.cs
@@ -13,73 +13,73 @@ namespace Apollo.AIM.SNAP.Web.Controls
 			_password.Text = "Password1";
 		}
 		
-		private void DisplayMessage(string message, bool isError)
-		{
-			_loginMessage.Text = message;
-			_loginMessageContainer.Visible = true;
+//        private void DisplayMessage(string message, bool isError)
+//        {
+//            _loginMessage.Text = message;
+//            _loginMessageContainer.Visible = true;
 			
-			if (isError) _loginMessage.CssClass += " csm_error_text";
-		}
+//            if (isError) _loginMessage.CssClass += " csm_error_text";
+//        }
 		
-		protected void _submitLogin_Click(object sender, EventArgs e)
-        {
-			if (!IsAuthenticatedUser(_networkId.Text, _password.Text))
-			{
-				DisplayMessage(@"Unable to Login.  Please re-enter your Network Login and Password. [Authentication Failure]", true);
-			}
-			else
-			{
-				Session.Clear();
+//        protected void _submitLogin_Click(object sender, EventArgs e)
+//        {
+//            if (!IsAuthenticatedUser(_networkId.Text, _password.Text))
+//            {
+//                DisplayMessage(@"Unable to Login.  Please re-enter your Network Login and Password. [Authentication Failure]", true);
+//            }
+//            else
+//            {
+//                Session.Clear();
 				
-				// TODO: make sure networkid is validated client-side so no nulls here
-				string networkId = _networkId.Text.ToLower().Trim();
+//                // TODO: make sure networkid is validated client-side so no nulls here
+//                string networkId = _networkId.Text.ToLower().Trim();
 
-				SnapUser currentUser = new SnapUser(networkId);
-				SnapSession.CurrentUser = currentUser;
+//                SnapUser currentUser = new SnapUser(networkId);
+//                SnapSession.CurrentUser = currentUser;
 				
-				string redirectUrl = "index.aspx";
+//                string redirectUrl = "index.aspx";
 				
-				if (!string.IsNullOrEmpty(Request.QueryString.ToString()))
-				{
-					redirectUrl += "?" + Request.QueryString.ToString();
-				}
-				else
-				{
-					if (_loginPathSelection.Value == "request_form" || _loginPathSelection.Value == "proxy_request")
-					{
-						redirectUrl += "?" + QueryStringConstants.REQUESTED_VIEW_INDEX + "=" + (int)ViewIndex.request_form;
-					}
+//                if (!string.IsNullOrEmpty(Request.QueryString.ToString()))
+//                {
+//                    redirectUrl += "?" + Request.QueryString.ToString();
+//                }
+//                else
+//                {
+//                    if (_loginPathSelection.Value == "request_form" || _loginPathSelection.Value == "proxy_request")
+//                    {
+//                        redirectUrl += "?" + QueryStringConstants.REQUESTED_VIEW_INDEX + "=" + (int)ViewIndex.request_form;
+//                    }
 
-					if (_loginPathSelection.Value == "request_form") 
-					{	
-						SnapSession.IsRequestPrePopulated = true;
-					}
-				}
+//                    if (_loginPathSelection.Value == "request_form") 
+//                    {	
+//                        SnapSession.IsRequestPrePopulated = true;
+//                    }
+//                }
 				
-				WebUtilities.Redirect(redirectUrl , false);
-			}
-        }
+//                WebUtilities.Redirect(redirectUrl , false);
+//            }
+//        }
         
-        private bool IsAuthenticatedUser(string networkId, string password)
-        {
-#if DEBUG
-	return true;
-#endif			
-			try
-			{
-				LdapAuthentication authentication = new LdapAuthentication();
+//        private bool IsAuthenticatedUser(string networkId, string password)
+//        {
+//#if DEBUG
+//    return true;
+//#endif			
+//            try
+//            {
+//                LdapAuthentication authentication = new LdapAuthentication();
 				
-				if (authentication.IsAuthenticated(networkId.ToLower().Trim(), password.Trim()))
-				{
-					return true;
-				}
-			}
-			catch (Exception ex)
-			{
-				// TODO: Logger.Error("LoginView > AuthenticateUser", ex);
-			}
+//                if (authentication.IsAuthenticated(networkId.ToLower().Trim(), password.Trim()))
+//                {
+//                    return true;
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                // TODO: Logger.Error("LoginView > AuthenticateUser", ex);
+//            }
 			
-			return false;
-        }
+//            return false;
+//        }
 	}
 }
