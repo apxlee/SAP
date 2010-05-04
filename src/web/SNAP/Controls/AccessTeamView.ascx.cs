48a1b35518e034e00a6d71ecea06e15b8c6f2133
clschwim <clschwim@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-05-04 21:26:38 +0000

    added approved to request filter
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@4730 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/AccessTeamView.ascx.cs
+++ src/web/SNAP/Controls/AccessTeamView.ascx.cs
@@ -36,6 +36,7 @@ namespace Apollo.AIM.SNAP.Web.Controls
             //int total = filterList[0] + filterList[1] + filterList[2];
             _pendingacknowledgementFilter.Text = "Pending Acknowledgement"; //(" + filterList[0].ToString() + ")";
             _pendingworkflowFilter.Text = "Pending Workflow"; //(" + filterList[1].ToString() + ")";
+            _approvedFilter.Text = "Approved";
             _pendingprovisioningFilter.Text = "Pending Provisioning"; //(" + filterList[2].ToString() + ")";
             _showallFilter.Text = "Show All"; //(" + total.ToString() + ")";
         }
