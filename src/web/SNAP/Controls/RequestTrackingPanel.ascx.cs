430cd957b3ecd2a02025c1b3522aac35d1cf8790
clschwim <clschwim@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-06-03 23:28:25 +0000

    updated to order by workflowStateId
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5191 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/RequestTrackingPanel.ascx.cs
+++ src/web/SNAP/Controls/RequestTrackingPanel.ascx.cs
@@ -266,7 +266,7 @@ namespace Apollo.AIM.SNAP.Web.Controls
 
             var wfDetails = Common.Request.WfDetails(RequestState);
             var details = wfDetails.Where(x => x.requestId.ToString() == RequestId)
-                          .OrderByDescending(o => o.pkId).Reverse();
+                          .OrderByDescending(o => o.workflowStateId).Reverse();
 
             foreach (usp_open_my_request_workflow_detailsResult list in details)
             {
