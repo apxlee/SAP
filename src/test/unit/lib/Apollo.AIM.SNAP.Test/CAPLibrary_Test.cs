69e7a98e7439d61c1f6ada8c87b2444441354a33
pxlee <pxlee@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-06-24 18:09:57 +0000

    test
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5317 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/test/unit/lib/Apollo.AIM.SNAP.Test/CAPLibrary_Test.cs
+++ src/test/unit/lib/Apollo.AIM.SNAP.Test/CAPLibrary_Test.cs
@@ -16,11 +16,30 @@ using DirectoryServices=Apollo.CA.DirectoryServices;
 
 namespace Apollo.AIM.SNAP.Test
 {
+    [TestFixture]
     public class CapLibraryTest
     {
         [SetUp]
         public void SetUp()
         {
+        }
+
+        [Test]
+        public void ShouldQueryActiveDirectory()
+        {
+            var ldap = new LDAPConnection();
+            Console.WriteLine("host: " + ldap.Host);
+            Console.WriteLine("base: " + ldap.LDAPbase);
+            Console.WriteLine("connection: " + ldap.LdapConnectionString);
+            Console.WriteLine("ldap host: " + ldap.LDAPHost);
+            Console.WriteLine("password" + ldap.Password);
+            Console.WriteLine("port: " + ldap.Port);
+            Console.WriteLine("username: " + ldap.Username);
+            Console.WriteLine("SearchBase: " + ldap.UserSearchBase);
+
+            Console.WriteLine("a.pxlee DN: " + Apollo.CA.DirectoryServices.GetUserDNByID("a.pxlee"));
+            //Console.WriteLine("auth: " + DirectoryServices.IsAuthenticated("a.pxlee", "abc"));
+            //Console.WriteLine("ADAM Group count: " + (DirectoryServices.getADAMGroups("a.pxlee")).Count);
         }
 
         //[Test]
@@ -29,13 +48,13 @@ namespace Apollo.AIM.SNAP.Test
         //    Assert.IsTrue(ConfigurationManager.AppSettings["ServiceDesk.ServiceDeskWebService.USD_WebService"] != string.Empty, "Should read from machine.config");
         //}
 
-        [Test]
-        public void ShouldHaveLogger()
-        {
-            // make sure log4net is configured
-            // c:/CapLogs with proper config files are there
-            Logger.Debug("This is logger test");
-        }
+        //[Test]
+        //public void ShouldHaveLogger()
+        //{
+        //    // make sure log4net is configured
+        //    // c:/CapLogs with proper config files are there
+        //    Logger.Debug("This is logger test");
+        //}
 
         //[Test]
         //public void ShouldBeReadyForServiceDesck()
@@ -62,46 +81,29 @@ namespace Apollo.AIM.SNAP.Test
         //    Console.WriteLine(sdConfigData);
         //}
 
-        [Test]
-        [Ignore]
-        public void ShouldCreateSDTicket()
-        {
-            var changeRequest = new ServiceDesk.ChangeRequest(SDConfig.Instance.Login, SDConfig.Instance.Password);
+        //[Test]
+        //[Ignore]
+        //public void ShouldCreateSDTicket()
+        //{
+        //    var changeRequest = new ServiceDesk.ChangeRequest(SDConfig.Instance.Login, SDConfig.Instance.Password);
 
-            changeRequest.CategoryName = "Server.Systems.Privileged Access";
+        //    changeRequest.CategoryName = "Server.Systems.Privileged Access";
 
-            changeRequest.Submitter.Get("svc_Cap");
-            changeRequest.AffectedUser.Get("pxlee");
+        //    changeRequest.Submitter.Get("svc_Cap");
+        //    changeRequest.AffectedUser.Get("pxlee");
 
-            changeRequest.Attributes["description"] = "Here is the description";
-            //changeRequest.Properties.SetPropertyValue("key1", "value");
-            //changeRequest.Properties.SetPropertyValue("key2", "value");
+        //    changeRequest.Attributes["description"] = "Here is the description";
+        //    //changeRequest.Properties.SetPropertyValue("key1", "value");
+        //    //changeRequest.Properties.SetPropertyValue("key2", "value");
 
-            changeRequest.Create();
-            Console.WriteLine(changeRequest.Number);
-            Assert.IsTrue(changeRequest.Number != string.Empty);
-
-
-        }
+        //    changeRequest.Create();
+        //    Console.WriteLine(changeRequest.Number);
+        //    Assert.IsTrue(changeRequest.Number != string.Empty);
 
 
-        [Test]
-        public void ShouldQueryActiveDirectory()
-        {
-            var ldap = new LDAPConnection();
-            Console.WriteLine("host: " + ldap.Host);
-            Console.WriteLine("base: " + ldap.LDAPbase);
-            Console.WriteLine("connection: " + ldap.LdapConnectionString);
-            Console.WriteLine("ldap host: " + ldap.LDAPHost);
-            Console.WriteLine("password" + ldap.Password);
-            Console.WriteLine("port: " + ldap.Port);
-            Console.WriteLine("username: " + ldap.Username);
-            Console.WriteLine("SearchBase: " + ldap.UserSearchBase);
+        //}
 
-            Console.WriteLine("a.pxlee DN: " + Apollo.CA.DirectoryServices.GetUserDNByID("a.pxlee"));
-            //Console.WriteLine("auth: " + DirectoryServices.IsAuthenticated("a.pxlee", "abc"));
-            //Console.WriteLine("ADAM Group count: " + (DirectoryServices.getADAMGroups("a.pxlee")).Count);
-        }
+
 
         //[Test]
         //public void ShouldSendEmail()
