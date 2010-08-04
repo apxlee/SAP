70e3eead7aaf017c37f712628fe557b2f9481fd1
pxlee <pxlee@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-08-04 17:55:29 +0000

    try to make ncover happy again
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5524 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/lib/Apollo.AIM.SNAP/Model/Utilities.cs
+++ src/lib/Apollo.AIM.SNAP/Model/Utilities.cs
@@ -8,7 +8,7 @@ using Apollo.CA.Logging;
 
 namespace Apollo.AIM.SNAP.Model
 {
-	class Utilities
+	public class Utilities
 	{
 		public static string WebRootUrl
 		{
@@ -40,29 +40,5 @@ namespace Apollo.AIM.SNAP.Model
 			}
 		}		
 
-		//private static void ConfigPerEnvironment(long requestId, string pageName)
-		//{
-		//    _imageUrl = @"http://";
-		//    _followLinkUrl = @"http://";
-
-		//    if (Environment.UserDomainName.ToUpper().Contains("DEV"))
-		//    {
-		//        _imageUrl += Environment.MachineName + ".devapollogrp.edu/snap/images";
-		//        _followLinkUrl += (Environment.MachineName + ".devapollogrp.edu/snap/" + pageName + ".aspx?RequestId=" + requestId);
-		//    }
-		//    else if (Environment.UserDomainName.ToUpper().Contains("QA"))
-		//    {
-		//        _imageUrl += "access.qaapollogrp.edu/snap/images";
-		//        _followLinkUrl += ("access.qaapollogrp.edu/snap/" + pageName + ".aspx?RequestId=" + requestId);
-		//    }
-		//    else
-		//    {
-		//        _imageUrl += "access.apollogrp.edu/snap/images";
-		//        _followLinkUrl += ("access.apollogrp.edu/snap/" + pageName + ".aspx?RequestId=" + requestId);
-		//    }
-
-		//    _imageUrl = "http://dwaxulbp001.devapollogrp.edu/snap/images";
-		//    _followLinkUrl = "http://dwaxulbp001.devapollogrp.edu/snap/" + pageName + ".aspx?RequestId=" + requestId;
-		//}		
 	}
 }
---
+++ src/test/unit/lib/Apollo.AIM.SNAP.Test/Utilities_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apollo.AIM.SNAP.Model;
+using NUnit.Framework;
+
+
+namespace Apollo.AIM.SNAP.Test
+{
+    [TestFixture]
+    public class Utilities_Test
+    {
+        [Test]
+        public void ShouldGetAbsPath()
+        {
+            Assert.IsTrue(Utilities.AbsolutePath == string.Empty);
+        }
+
+    }
+}
