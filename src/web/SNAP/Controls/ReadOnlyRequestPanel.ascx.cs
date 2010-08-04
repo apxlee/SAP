aa50882d57ab8f6fc42d36ff3b96efb76f913136
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-08-04 19:07:33 +0000

    added REQID, USERID, MGRUSERID to ReadOnlyRequestPanel > CompareManagerName
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5529 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Controls/ReadOnlyRequestPanel.ascx.cs
+++ src/web/SNAP/Controls/ReadOnlyRequestPanel.ascx.cs
@@ -54,7 +54,7 @@ namespace Apollo.AIM.SNAP.Web.Controls
 				}
 				catch (Exception ex)
 				{
-					Logger.Error("ReadOnlyRequestPanel - GetFullNameFromAD [USERID:" + userId + ", REQID:" + RequestId+ "]:\r\n" + ex.Message + "\r\nStackTrace: " + ex.StackTrace);
+					Logger.Error("ReadOnlyRequestPanel > GetFullNameFromAD [USERID:" + userId + ", REQID:" + RequestId+ "]:\r\nMessage:" + ex.Message + "\r\nStackTrace: " + ex.StackTrace);
 					return "<span class=\"csm_error_text\">Unknown (Active Directory Error)</strong></span>";
 				}
 			}
@@ -72,7 +72,7 @@ namespace Apollo.AIM.SNAP.Web.Controls
 			}
 			catch (Exception ex)
 			{
-                Logger.Error("ReadOnlyRequestPanel - CompareManagerName, Message:" + ex.Message + ", StackTrace: " + ex.StackTrace);
+				Logger.Error("ReadOnlyRequestPanel > CompareManagerName [USERID:" + userId + ", MGRUSERID:" + mgrUserId + ", REQID:" + RequestId + "]:\r\nMessage" + ex.Message + "\r\nStackTrace: " + ex.StackTrace);
 			}
 
 			return string.Empty;
