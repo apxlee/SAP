61ff1284530320bc7f7623c15cae6cc3b503fa71
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-10-06 22:40:34 +0000

    cleanup old code
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5812 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/web/SNAP/Common/Constants.cs
+++ src/web/SNAP/Common/Constants.cs
@@ -9,7 +9,6 @@ namespace Apollo.AIM.SNAP.Web.Common
 	{
 		public const string DEMONSTRATION_ROLE = "role";
 		public const string REQUEST_ID = "requestId";
-		//public const string REQUESTED_VIEW_INDEX = "viewIndex"; // remove
 		public const string REFERRING_URL = "refurl";
 		public const string ERROR_REASON = "errorReason";
 	}
@@ -19,23 +18,7 @@ namespace Apollo.AIM.SNAP.Web.Common
 		public const string CURRENT_USER = "CurrentUser";
 		public const string IS_USER_CREATED = "IsUserCreated";
 		public const string SELECTED_REQUEST_ID = "SelectedRequestId";
-		//public const string REQUESTED_VIEW_INDEX = "RequestedViewIndex"; // remove
 		public const string IS_REQUEST_PREPOPULATED = "IsRequestPrepopulated";
 		public const string REQUESTED_PAGE = "RequestedPage";
 	}
-
-    /*
-	public static class PageNames
-	{
-		public const string ACCESS_TEAM = "AccessTeam";
-		public const string APP_ERROR = "AppError";
-		public const string APP_MAINTENANCE = "AppMaintenance";
-		public const string DEFAULT_LOGIN = "Default";
-		public const string APPROVING_MANAGER = "MyApprovals";
-		public const string USER_VIEW = "MyRequests";
-		public const string REQUEST_FORM = "RequestForm";
-		public const string SEARCH = "Search";
-		public const string SUPPORT = "Support";
-	}
-     */
 }
