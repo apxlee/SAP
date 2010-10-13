5760918dcb4fd41025922e1b6a6fa8b9f9b53c3b
jdsteele <jdsteele@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-10-13 21:02:01 +0000

    added )
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5850 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- src/lib/Apollo.AIM.SNAP.Process/ReminderTask.cs
+++ src/lib/Apollo.AIM.SNAP.Process/ReminderTask.cs
@@ -79,7 +79,7 @@ namespace Apollo.AIM.SNAP.Process
                     || wfState.workflowStatusEnum == (byte)WorkflowState.Change_Requested))
             {
                 var accessReq = new AccessRequest(wfState.SNAP_Workflow.requestId);
-                accessReq.NoAccess(WorkflowAction.Abandon, string.Format("Request has been abandoned for inactivity.  Maximum overdue days ({0}) for action reached.", overdueAlertMaxDay);
+                accessReq.NoAccess(WorkflowAction.Abandon, string.Format("Request has been abandoned because of inactivity.  Maximum overdue days ({0}) for action reached.", overdueAlertMaxDay));
                 return false;
             }
 
