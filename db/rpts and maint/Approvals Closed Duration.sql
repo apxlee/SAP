4ad54c2003f0b739f400091bb9762c59beeef3e9
clschwim <clschwim@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-12-02 22:07:25 +0000

    updated to monthly report
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5963 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- db/rpts and maint/Approvals Closed Duration.sql
+++ db/rpts and maint/Approvals Closed Duration.sql
@@ -1,16 +1,65 @@
---//Approvals Closed Duration
-
 USE [BPS_SupplementalAccess]
 
-select f.requestId, r.userDisplayName, a.displayName as [Approver Name], s.notifyDate, s.dueDate, s.completedDate,
-(DateDiff(d,s.dueDate, s.completedDate) - (select count(dayOfWeekDate) from dbo.SNAP_Weekends_and_Holidays
-where dayOfWeekDate between s.dueDate and s.completedDate)) as [Days Late]
-            from dbo.SNAP_Requests r, SNAP_Workflow f, SNAP_Workflow_State s, dbo.SNAP_Actors a
-            where (r.statusEnum = 3)
-                  and r.pkid = f.requestId 
-                  and f.pkid = s.workflowId
-                  and f.actorid = a.pkid
-				  and f.actorid <> 1
-                  and s.workflowStatusEnum = 0
-                  and completedDate is not null
-            order by a.displayName, r.pkId
+DECLARE @monthStart smalldatetime
+DECLARE @monthEnd smalldatetime
+
+SET @monthStart = convert(smalldatetime, '11/01/2010', 101)
+SET @monthEnd = convert(smalldatetime, '11/30/2010', 101)
+
+CREATE TABLE #approvalTotals
+(
+	pkId INT,
+	displayName nvarchar(128),
+	daysLate decimal(18,2)
+)
+insert into #approvalTotals(pkId,displayName,daysLate)
+select r.pkId as pkId, a.displayName as displayName,
+(DateDiff(d,ws.dueDate, ws.completedDate) - (select count(dayOfWeekDate) from dbo.SNAP_Weekends_and_Holidays
+where dayOfWeekDate between ws.dueDate and ws.completedDate) + (select count(dayOfWeekDate) from dbo.SNAP_Weekends_and_Holidays
+where dayOfWeekDate between ws.completedDate and ws.dueDate)) as daysLate
+from dbo.SNAP_Requests r
+join dbo.SNAP_Workflow w on r.pkId = w.requestId
+join dbo.SNAP_Workflow_State ws on w.pkId = ws.workflowId
+join dbo.SNAP_Actors a on w.actorId = a.pkId
+where a.pkId <> 1 
+and ws.pkId IN(select max(subWs.pkId) from dbo.SNAP_Workflow subW
+join dbo.SNAP_Workflow_State subWs on subW.pkId = subWs.workflowId
+join dbo.SNAP_Actors subA on subW.actorId = subA.pkId
+where subW.requestId = w.requestId
+and subWs.workflowStatusEnum NOT IN(5,7)
+and subA.userId = a.userId)
+and ws.completedDate between @monthStart and @monthEnd
+group by r.pkId,a.displayName,ws.dueDate, ws.completedDate
+
+DECLARE @totalAverage as decimal(18,2)
+
+select @totalAverage = (select AVG(daysLate) from #approvalTotals)
+
+select displayName as [Approver Name],
+count(pkId) as [Total Requests],
+CAST(AVG(daysLate) as decimal(18,2)) as [Average Days],
+CAST((@totalAverage - AVG(daysLate)) as decimal(18,2)) as [Approver Rating]
+from #approvalTotals
+group by displayName
+order by displayName
+
+DROP TABLE #approvalTotals
+
+select r.pkId as pkId, a.displayName as displayName,
+(DateDiff(d,ws.dueDate, ws.completedDate) - (select count(dayOfWeekDate) from dbo.SNAP_Weekends_and_Holidays
+where dayOfWeekDate between ws.dueDate and ws.completedDate) + (select count(dayOfWeekDate) from dbo.SNAP_Weekends_and_Holidays
+where dayOfWeekDate between ws.completedDate and ws.dueDate)) as daysLate
+from dbo.SNAP_Requests r
+join dbo.SNAP_Workflow w on r.pkId = w.requestId
+join dbo.SNAP_Workflow_State ws on w.pkId = ws.workflowId
+join dbo.SNAP_Actors a on w.actorId = a.pkId
+where a.pkId <> 1 
+and ws.pkId IN(select max(subWs.pkId) from dbo.SNAP_Workflow subW
+join dbo.SNAP_Workflow_State subWs on subW.pkId = subWs.workflowId
+join dbo.SNAP_Actors subA on subW.actorId = subA.pkId
+where subW.requestId = w.requestId
+and subWs.workflowStatusEnum NOT IN(5,7)
+and subA.userId = a.userId)
+and ws.completedDate between @monthStart and @monthEnd
+group by r.pkId,a.displayName,ws.dueDate, ws.completedDate
+order by a.displayName
--- db/rpts and maint/Outstanding Approval Duration.sql
+++ db/rpts and maint/Outstanding Approval Duration.sql
@@ -2,7 +2,13 @@
 
 USE [BPS_SupplementalAccess]
 DECLARE @today as smalldatetime
+
+DECLARE @monthStart smalldatetime
+DECLARE @monthEnd smalldatetime
+
 SET @today = getdate() 
+SET @monthStart = convert(smalldatetime, '11/01/2010', 101)
+SET @monthEnd = convert(smalldatetime, '11/30/2010', 101)
 
 select f.requestId as [RequestId], a.displayName as [Approver Name], (DateDiff(d,s.dueDate, @today) - (select count(dayOfWeekDate) from dbo.SNAP_Weekends_and_Holidays
 where dayOfWeekDate between s.dueDate and @today)) as [Days Late],
@@ -13,6 +19,7 @@ and r.pkid = f.requestId
 and f.pkid = s.workflowId
 and f.actorid = a.pkid
 and s.workflowStatusEnum = 7
+and s.dueDate between @monthStart and @monthEnd
 and completedDate is null
 and (DateDiff(d,s.dueDate, @today) - (select count(dayOfWeekDate) from dbo.SNAP_Weekends_and_Holidays
 where dayOfWeekDate between s.dueDate and @today)) > 0
