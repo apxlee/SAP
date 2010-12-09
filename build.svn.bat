5828558784eb9429ea5dee26c09b463b9d403e7d
pxlee <pxlee@baa2f1a4-ed17-0410-b6b6-a0ee3716f956>
2010-12-09 16:33:03 +0000

    add ver # in argument
    git-svn-id: http://svn.apollogrp.edu/idm/snap/trunk@5996 baa2f1a4-ed17-0410-b6b6-a0ee3716f956

--- build.svn.bat
+++ build.svn.bat
@@ -1,2 +1,11 @@
-nant -f:release\buildfiles\default.build -D:version=0.0.0 build.svn package
+@echo off
+set ver=%1
+
+if %1.==. (
+set ver=0.0.0
+)else (
+echo Build version: %ver%
+)
+
+nant -f:release\buildfiles\default.build -D:version=%ver% build.svn package
 pause
